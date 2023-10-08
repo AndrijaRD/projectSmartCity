ed36eee24f33fc897c7c09e5632f63877919680e
andrijard <radivojevandrija007@gmail.com>
2023-10-08 11:59:01 +0200

    harware weather_sensor

---
+++ hardware/weather_sensor/weather_sensor.ino
@@ -0,0 +1,36 @@
+#include <sc.h>
+#include <DHT.h>
+
+#define DHTPIN D4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+// TEST VALUES
+float temperature = 38.75;
+float humidity = 13.2;
+
+const String id = "WTHSEN_000001"
+
+// API HOST URL
+const String host = "http://192.168.0.103:4001";
+
+void setup() {
+  Serial.begin(115200);                         // START SERIAL COMMUNICATION
+  sc::ESP_WiFi::connect("dlink", "Mpupin2019"); // CONNECT TO WIFI
+  dht.begin();                                  // START SENSOR OBJECT
+}
+void loop() {
+  humidity = dht.readHumidity();                // READ HUMIDITY     [ %  ]
+  temperature = dht.readTemperature();          // READ TEMPERATURE  [ °C ]
+  if (isnan(temperature) || isnan(humidity))
+    Serial.println("Failed to read from DHT sensor!");
+  else
+    sendData();                                   // SEND DATA TO API
+  delay(1000);                               // 30min DELAY BETWEEN MESURMENTS
+}
+
+void sendData(){
+  sc::HttpClient client;                                        // HTTP CLIENT
+  client.setHost(host);                                         // SET API HOST URL
+  client.get("/sensor/weather/insert/" + id + "/" + String(temperature) + "/" + String(humidity)); // FETCH THE URL WITH MESURED DATA IN ENDPOINT
+}
