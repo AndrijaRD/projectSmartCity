846c32d54faee2c6bc86189bb8a64b53645d0e06
andrijard <radivojevandrija007@gmail.com>
2023-10-08 18:44:15 +0200

    setup

---
+++ app/src/Components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect, use } from "react";
+
+export default function Home() {
+
+    return(
+        <main className="home">
+            Home
+        </main>
+    );
+}
---
+++ app/src/Components/Weather/Weather.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect, use } from "react";
+
+export default function Weather(){
+    return(
+        <main className="weather">
+            Weather
+        </main>
+    );
+}
