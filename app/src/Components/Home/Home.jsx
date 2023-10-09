d60f90ddaa7fc4426d0addd47e3e4bcca8c04763
andrijard <radivojevandrija007@gmail.com>
2023-10-09 22:07:26 +0200

    home page link to weather route

--- app/src/Components/Home/Home.jsx
+++ app/src/Components/Home/Home.jsx
@@ -5,6 +5,7 @@ export default function Home() {
     return(
         <main className="Home">
             <h1>Home Page</h1>
+            <h3 style={{margin: "10px 20px"}}>Go to <a href='/weather'>weather app</a>.</h3>
         </main>
     );
 }
