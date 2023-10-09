4334d7120c7082ab3ae6a810b3d43e4ad08ebd83
andrijard <radivojevandrija007@gmail.com>
2023-10-09 13:06:25 +0200

    finished desing

--- app/src/Components/Weather/Weather.jsx
+++ app/src/Components/Weather/Weather.jsx
@@ -39,8 +39,81 @@ function Dashboard(){
                         <img src={`/assets/weatherTypes/${weatherType}.png`} alt="" />
                     </div>
                 </div>
+
+
+
                 <div className="bottom">
-                    
+                    <div className="mainMeasurement measurement">
+                        <div className="data">
+                            <div className="left">
+                                <img src="/assets/measurementIcons/humidity.png" alt="" />
+                            </div>
+                            <div className="right">
+                                <h3 className="title">Humidity:</h3>
+                                <h3>22%</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mainMeasurement measurement">
+                        <div className="data">
+                            <div className="left">
+                                <img src="/assets/measurementIcons/wind.png" alt="" />
+                            </div>
+                            <div className="right">
+                                <h3 className="title">Wind:</h3>
+                                <h3>13m/s</h3>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="measurement">
+                        <div className="data">
+                            <div className="left">
+                                <img src="/assets/measurementIcons/gas.png" alt="" />
+                            </div>
+                            <div className="right">
+                                <h3 className="title">Gas:</h3>
+                                <h3>7m3</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="measurement">
+                        <div className="data">
+                            <div className="left">
+                                <img src="/assets/measurementIcons/methane.png" alt="" />
+                            </div>
+                            <div className="right">
+                                <h3 className="title">Methane:</h3>
+                                <h3>22mol</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="measurement">
+                        <div className="data">
+                            <div className="left">
+                                <img src="/assets/measurementIcons/radiation.png" alt="" />
+                            </div>
+                            <div className="right">
+                                <h3 className="title">Radiation:</h3>
+                                <h3>22Pa</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="measurement">
+                        <div className="data">
+                            <div className="left">
+                                <img src="/assets/measurementIcons/toxic.png" alt="" />
+                            </div>
+                            <div className="right">
+                                <h3 className="title">Toxic Particles:</h3>
+                                <h3>0.2mol</h3>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="week">
