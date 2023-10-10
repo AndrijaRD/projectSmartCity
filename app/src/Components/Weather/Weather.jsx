294ec773c33824ab2fd4b514f4ce03750ceadc55
andrijard <radivojevandrija007@gmail.com>
2023-10-10 22:45:30 +0200

    button action on Home.jsx, and installed map lib

--- app/src/Components/Weather/Weather.jsx
+++ app/src/Components/Weather/Weather.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css'
 import './style/Weather.css'
 
 import Sidebar from "../Sidebar/Sidebar";
@@ -6,7 +8,6 @@ import Sidebar from "../Sidebar/Sidebar";
 export default function Weather(){
     const [mode, setMode] = useState("Dashboard");
     
-
     return(
         <main className="Weather">
             <Sidebar mode={mode} setMode={setMode} />
@@ -87,11 +88,30 @@ function Dashboard(){
 }
 
 function Map(){
-    return(
-        <main className="mainContent">
-            Map
-        </main>
-    )
+    const myLocation = [44.865749, 20.642635]
+    const markerIcon2 = L.icon({
+        iconUrl: "/assets/location.png",
+        iconSize: [32, 32], // Size of the icon
+        iconAnchor: [16, 32], // Point of the icon that corresponds to the marker's location
+        popupAnchor: [0, -32] // Point from which the popup should open relative to the iconAnchor
+    });
+
+    const markerIcon = L.divIcon({
+        className: 'custom-icon',
+        html: '<div>Marker Text</div>',
+        iconSize: [80, 40],
+        iconAnchor: [40, 20]
+      });
+
+    useEffect(() => {
+        const map = L.map('map').setView(myLocation, 17);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '© OpenStreetMap contributors'}).addTo(map);
+        L.marker([44.865749, 20.642635], {icon: markerIcon}).addTo(map)
+
+        return () => map.remove()
+    }, [])
+
+    return <div id="map" style={{ height: '96vh', width: '100%' }}></div>
 }
 
 function Cities(){
