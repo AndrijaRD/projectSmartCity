2e0a49970abe39c4c51cc9d6cde72c509824c7ef
andrijard <radivojevandrija007@gmail.com>
2023-10-10 14:08:49 +0200

    added alt text to image, warnings are error

--- app/src/Components/404/404.jsx
+++ app/src/Components/404/404.jsx
@@ -4,7 +4,7 @@ export default function NotFound(){
     return(
         <main className="NotFound">
             <h3><a href='/'>Nazad Kuci</a></h3>
-            <img src="/assets/components/404.png" />
+            <img src="/assets/components/404.png" alt='404.png' />
             <div className="text">
                 <h1>404 Not Found</h1>
                 <h3>Stranica nije pronadjena. Ili ste usli na nepostojeci link ili se stranica idalje izgradjuje.</h3>
